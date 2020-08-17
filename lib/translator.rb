18afe3b1581285376173fa9970fac9dab3978787
beatific-chain-3778 <milobravman20@gmail.com>
2020-08-17 10:07:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,6 +8,7 @@ def load_library(lib)
   #superloop through NDS emotos and change each ary to a hash
   emotos.each do |key, ary|
     emotos[key] = { :english => ary[0], :japanese => ary[1] }
+    binding.pry
   end
   
 end
