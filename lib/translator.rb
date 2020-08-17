4d66744f3c27ba9906b45cfd7ba3ea2336e08524
beatific-chain-3778 <milobravman20@gmail.com>
2020-08-17 10:02:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,6 @@ def load_library(lib)
   emotos = YAML.load_file(lib)
   #superloop through NDS emotos and change each ary to a hash
   emotos.each do |key, ary|
-    ary 
     emotos[key] = {ary[0] => ary[1]}
   end
   binding.pry
