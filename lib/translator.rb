cf445e2b2fe50735e9baf05645dce952560c0118
beatific-chain-3778 <milobravman20@gmail.com>
2020-08-17 09:49:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,12 +1,15 @@
 # require modules here
 require "yaml"
-
+binding.pry
 
 def load_library(lib)
   # code goes here
   emotos = YAML.load_file(lib)
   #superloop through NDS emotos and change each ary to a hash
-  
+  emotos.each do |key, ary|
+    emotos[key] = {ary[0], ary[1]}
+    binding.pry
+  end
 end
 
 def get_japanese_emoticon
